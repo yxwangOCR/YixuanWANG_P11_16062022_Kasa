0a52cd0a5b7d0be3d5868b220e187418c07d54d6
yxwangOCR <wang.yixuan@live.fr>
2022-06-17 05:01:03 -0400

    components style

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,18 +6,32 @@ function Cards() {
   return (
     <div>
       <h1>Cards</h1>
-      {Data.map((item, index) => {
-        // get data from data.json
-        return (
-          <div key={(item.id)}>
-            <h4>{item.title}</h4>
-            <p>{item.description}</p>
-            <image>{item.cover}</image>
-          </div>
-        );
-      })}
+      <Wrapper>
+        {Data.map((item, index) => {
+          // get data from data.json
+          return (
+            <Card>
+              <div key={item.id}>
+                <h4>{item.title}</h4>
+                <p>{item.description}</p>
+                <image>{item.cover}</image>
+              </div>
+            </Card>
+          );
+        })}
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  margin: 0 auto;
+`;
+
+const Card = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 
 export default Cards;
